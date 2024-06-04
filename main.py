27d0b45a0296127fe7ae2441b13472c57c7313dc
Ahmed Hussein <wolktheline@yahoo.com>
2024-06-03 19:24:30 -0600

    Match

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 todos = []
 
-while 2 > 1:
-    todo = input("Enter the task: ")
-    todos.append(todo)
-    print(todos)
+while True:
+    users_input = input("Type add or show: ")
+    match users_input:
+        case "add":
+            todo = input("Enter the todo: ")
+            todos.append(todo.capitalize())
+        case "show":
+            print(todos)
+
 
 
---
+++ password.py
@@ -0,0 +1,13 @@
+# password = "ahmed!"
+#
+# user_input = ""
+#
+# while password != user_input:
+#     user_input = input("Enter the password: ")
+
+
+
+while True:
+    name = input("Enter your name: ")
+    print(name.capitalize())
+
