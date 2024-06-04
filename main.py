8799154d66a69d34e2f12ab09fd77245ea1d92f9
Ahmed Hussein <wolktheline@yahoo.com>
2024-06-03 19:43:55 -0600

    Using _ for any other case without specifics.

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 todos = []
 
 while True:
-    users_input = input("Type add or show: ")
+    users_input = input("Type add, show or exit: ").strip().lower()
     match users_input:
         case "add":
             todo = input("Enter the todo: ")
             todos.append(todo.capitalize())
         case "show":
-            print(todos)
+            for i in todos:
+                print(i)
+        case "exit":
+            print("Bye!")
+            break
+        case _:
+            print("Please enter a valid command!")
 
 
 
