62d328d08fd2008e5775e9055e4c3fe11cff787b
Ahmed Hussein <wolktheline@yahoo.com>
2024-06-03 20:35:32 -0600

    Edit one item in the list and save it.

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 todos = []
 
 while True:
-    users_input = input("Type add, show or exit: ").strip().lower()
+    users_input = input("Type add, show, edit or exit: ").strip().lower()
     match users_input:
         case "add":
             todo = input("Enter the todo: ")
             todos.append(todo.capitalize())
-        case "show":
+        case "show" | "display":
             for i in todos:
                 print(i)
+        case "edit":
+            number = int(input("Number of todo to edit?: ")) - 1
+            editable = todos[number]
+            new_value = input("Edit it: ")
+            todos[number] = new_value.capitalize()
         case "exit":
             print("Bye!")
             break
         case _:
             print("Please enter a valid command!")
+
+
+
 
 
 
