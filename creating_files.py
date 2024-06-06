b7f6ff83124565c09a9c64990a9ba2ff9b9f17af
Ahmed Hussein <wolktheline@yahoo.com>
2024-06-05 22:30:24 -0600

    Using zip() to get items from two lists

--- creating_files.py
+++ creating_files.py
@@ -1,7 +1,14 @@
-content = ["This is first file", "This is second file", "This is third file"]
+contents = ["This is first file", "This is second file", "This is third file"]
 
 files = ["first.txt", "second.txt", "third.txt"]
 
-for i in range(len(files)):
-    file = open(f"files/{files[i]}", "w")
-    file.write(content[i])
+# for i in range(len(files)):
+#     file = open(f"files/{files[i]}", "w")
+#     file.write(content[i])
+
+
+# Another way to do the same using zip()
+
+for file, content in zip(files, contents):
+    file = open(f"files/{file}", "w")
+    file.writelines(content)
