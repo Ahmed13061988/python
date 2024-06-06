1f4fa88f45aa6957f96c2069fb3e74c7b7d7c28b
Ahmed Hussein <wolktheline@yahoo.com>
2024-06-05 21:14:22 -0600

    Adding the todos list to a file, read the file everytime we want to access the list, this way we will keep track of every element we added to the text file.

---
+++ Example/alphabetically.py
@@ -0,0 +1,5 @@
+buttons = [('John', 'Sen', 'Morro'), ('Lin', 'Ajay', 'Filip')]
+for first, second, third in buttons:
+    print(first, second, third)
+
+
--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-todos = []
-
 while True:
-    users_input = input("Type add, show, edit, completed exit: ").strip().lower()
+    users_input = input("Type add, show, edit, complete or exit: ").strip().lower()
     match users_input:
         case "add":
-            todo = input("Enter the todo: ")
+            todo = input("Enter the todo: ") + "\n"
+            file = open("todos.txt", "r")
+            todos = file.readlines()
             todos.append(todo.capitalize())
+            file = open("todos.txt", 'w')
+            file.writelines(todos)
         case "show" | "display":
+            file = open("todos.txt", "r")
+            todos = file.readlines()
             for index, i in enumerate(todos):
                 print(f"{index + 1}-{i}")
         case "edit":
             number = int(input("Number of todo to edit?: ")) - 1
+            file = open("todos.txt", "r")
+            todos = file.readlines()
             editable = todos[number]
             new_value = input("Edit it: ")
             todos[number] = new_value.capitalize()
-        case "completed":
-            item = input("What number is done?: ").capitalize()
-            todos.remove(item)
+        case "complete":
+            item = int(input("What number is done?: "))
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            todos.pop(item-1)
             print(f"You're done with {item}")
         case "exit":
             print("Bye!")
