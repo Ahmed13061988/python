a506750bdbeb086a4c1b087d317fab1522ddf998
Ahmed Hussein <wolktheline@yahoo.com>
2024-06-10 19:12:20 -0600

    Functions

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
+def get_todos():
+    with open("files/todos.txt", "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
 while True:
     users_input = input("Type add, show, edit, complete or exit: ").strip().lower()
 
     if users_input.startswith("add"):
         todo = users_input.replace("add ", "")
-        with open("files/todos.txt", "r") as file:
-            todos = file.readlines()
+        todos = get_todos()
         todos.append(todo.capitalize() + "\n")
-        print(todos)
         with open("files/todos.txt", 'w') as file:
             file.writelines(todos)
 
     elif users_input.startswith("show"):
-        with open("files/todos.txt", "r") as file:
-            todos = file.readlines()
+        todos = get_todos()
         new_todos = [item.strip("\n") for item in todos]
         for index, i in enumerate(new_todos):
             print(f"{index + 1}-{i}")
@@ -21,9 +22,7 @@ while True:
         try:
             number = users_input[5:]
             number = int(number) - 1
-            with open("files/todos.txt", "r") as file:
-                todos = file.readlines()
-
+            todos = get_todos()
             new_value = input("Edit it: ") + '\n'
             todos[number] = new_value.capitalize()
             with open("files/todos.txt", 'w') as file:
@@ -35,9 +34,7 @@ while True:
         try:
             item = int(users_input[8:])
             item = item - 1
-
-            with open("files/todos.txt", "r") as file:
-                todos = file.readlines()
+            todos = get_todos()
             todos.pop(item)
             with open("files/todos.txt", "w") as file:
                 file.writelines(todos)
