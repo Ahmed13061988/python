1b2a2f6812544886e2ae5bd3a30a40c3f1ed00df
Ahmed Hussein <wolktheline@yahoo.com>
2024-06-27 18:52:02 -0600

    Module approach.

--- todos/main.py
+++ todos/main.py
@@ -1,13 +1,5 @@
-def get_todos(filepath='files/todos.txt'):
-    """Get the data from a file and then store it in a list"""
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="files/todos.txt"):
-    with open(filepath, 'w') as file_local:
-        file_local.writelines(todos_arg)
+from functions import get_todos, write_todos
+import functions
 
 
 while True:
