65d26044046a9c0233a121bb8ecdf54a31f5bb80
Ahmed Hussein <wolktheline@yahoo.com>
2024-07-09 22:12:18 -0600

    using FreeSimpleGUI library, creating a window with input box, label and an add button (Submit).

--- todos/GUI.py
+++ todos/GUI.py
@@ -1,1 +1,9 @@
+import FreeSimpleGUI as fs
 import functions
+
+label = fs.Text("Type a to-do")
+input_box = fs.InputText()
+button = fs.Submit("Add")
+window = fs.Window("To do App", layout=[[label], [input_box, button]])
+window.read()
+window.close()
