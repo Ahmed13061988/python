c24d9208c8fef542aafc3de3401528bb7a2bc44d
Ahmed Hussein <wolktheline@yahoo.com>
2024-07-11 20:46:01 -0600

    Made add button to add the entry to the text file.

---
+++ another_training/my_functions.py
@@ -0,0 +1,14 @@
+def say_hello():
+    print("Hello there!")
+
+
+def say_goodBye():
+    print("Bye")
+
+
+print("I want to live")
+
+if __name__ == "__main__":
+    print("execute")
+
+
--- todos/GUI.py
+++ todos/GUI.py
@@ -2,8 +2,24 @@ import FreeSimpleGUI as fs
 import functions
 
 label = fs.Text("Type a to-do")
-input_box = fs.InputText()
+input_box = fs.InputText(tooltip="Enter todo", key="todo")
 button = fs.Submit("Add")
-window = fs.Window("To do App", layout=[[label], [input_box, button]])
-window.read()
+window = fs.Window("To do App",
+                   layout=[[label], [input_box, button]],
+                   font=('Helvetica', 20))
+while True:
+    event, values = window.read()
+    todo_list = functions.get_todos()
+    match event:
+        case "Add":
+            todo_list.append(values["todo"] + "\n")
+            functions.write_todos(todo_list)
+        case fs.WIN_CLOSED:
+            break
+print(todo_list)
+
+
+
+
+
 window.close()
